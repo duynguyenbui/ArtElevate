54d3cbb70135e3464d9fa187ad3e54ca1f9abe5b
Duy Nguyen <buiduynguyen@Nguyens-MacBook-Pro.local>
2023-12-18 20:13:51 +0700

    Adding polly

--- SearchService/Program.cs
+++ SearchService/Program.cs
@@ -1,3 +1,6 @@
+using System.Net;
+using Polly;
+using Polly.Extensions.Http;
 using SearchService.Core;
 using SearchService.Data;
 using SearchService.Services;
@@ -7,14 +10,26 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddProblemDetails();
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
-builder.Services.AddHttpClient<AuctionServiceHttpClient>();
+builder.Services.AddHttpClient<AuctionServiceHttpClient>()
+    .AddPolicyHandler(GetPolicy());
 
 // Construct application
 var app = builder.Build();
 app.MapControllers();
 app.UseExceptionHandler();
 
-try { await DatabaseInitializer.InitializeDatabase(app); }
-catch (Exception e) { Console.WriteLine($"[DATABASE_SEEDING_FAILED] {e.Message}"); }
+app.Lifetime.ApplicationStarted.Register(async () =>
+{
+    try { await DatabaseInitializer.InitializeDatabase(app); }
+    catch (Exception e) { Console.WriteLine($"[DATABASE_SEEDING_FAILED] {e.Message}"); }
+});
+
+
 // Run application
 app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
