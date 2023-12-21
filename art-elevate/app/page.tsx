431a5895d77edb56cf1873924c80c84f1ae38b09
Duy Nguyen <buiduynguyen@Nguyens-MacBook-Pro.local>
2023-12-21 10:46:35 +0700

    Init NextJS Project

---
+++ art-elevate/app/page.tsx
@@ -0,0 +1,12 @@
+import { ModeToggle } from '@/components/mode-toggle';
+import React from 'react';
+
+const Page = () => {
+  return (
+    <div>
+      <ModeToggle />
+    </div>
+  );
+};
+
+export default Page;
